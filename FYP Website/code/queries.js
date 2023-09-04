4d035d355a02993483b5f4afa4a6f20b8bc613fc
Chi Hui <chihui8199@gmail.com>
2023-09-04 17:52:08 +0800

    modify technical skills query

--- FYP Website/code/queries.js
+++ FYP Website/code/queries.js
@@ -18,22 +18,25 @@ async function read(cypher, params = {}) {
     const res = await session.executeRead(tx => tx.run(cypher, params))
    // 3. Write the result to a file
    const values = await res.records.map(record => record.toObject())
-   const results = await res.records.map(record => record.toObject()).map(item => ({
+   //const result = await read(query)
+  const finalRes = res.records
+    .map(record => record.toObject())
+    .map(item => ({
       tscKeyID: item?.tscKeyID?.low,
-      proficiencyLevel: item?.proficiencyLevels.map(knowledge=> {
+      proficiencyLevel: item?.proficiencyLevels.map(ability => {
         return {
-          proficiencyLevel: knowledge.proficiencyLevel.low,
-          filteredKnowledge: knowledge.filteredKnowledge.filter(knowledge => !!knowledge)
+          proficiencyLevel: ability.proficiencyLevel.low,
+          filteredAbility: ability.filteredAbility.filter(ability => !!ability)
         }
       }),
       tscDescrip: item.t.properties['TSC Description'],
       tscCategory: item.t.properties['TSC Category'],
-      tscTitle: item.t.properties['TSC Title'],
-   }))
-   console.log(results)
+      tscTitle: item.t.properties['TSC Title']
+    }))
+   //console.log(results)
   
-   const resultString = JSON.stringify(results, null, 2); // Convert the result to a pretty-printed JSON string
-   fs.writeFileSync('abc.json', resultString);
+   const resultString = JSON.stringify(finalRes, null, 2); // Convert the result to a pretty-printed JSON string
+   fs.writeFileSync('result.json', resultString);
     return res
   } finally {
     // 4. Close the session
@@ -46,24 +49,24 @@ async function read(cypher, params = {}) {
 //RETRIEVE ALL TSC KEY IDS by passing in the jobs and getting the unique TSC Key IDs for all jobs passed in
 const tscKeyIDs = [1, 2, 3];
 const query = `
-MATCH (j:Job)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
-WHERE j.jobid IN [1375, 1038, 757]
-WITH jt.\`TSC Key ID\` AS TSCKeyID, k.\`TSC Title\` AS title
-ORDER BY TSCKeyID DESC
-
-WITH COLLECT(DISTINCT TSCKeyID) AS requiredProficiency // Doesn't care what level of prof first
-MATCH (n:TSCProficiency)
-WHERE n.\`TSC Key ID\` IN requiredProficiency
-
-WITH n.\`TSC Key ID\` AS tscKeyID, n.\`Proficiency Level\` AS proficiencyLevel, COLLECT(DISTINCT n.\`Knowledge\`) AS distinctKnowledge
-
-// Group by TSC Key ID and Proficiency Level
-WITH tscKeyID, proficiencyLevel, [x IN distinctKnowledge WHERE x IS NOT NULL | x] AS filteredKnowledge
-
-// Collect results by TSC Key ID
-WITH tscKeyID, COLLECT({proficiencyLevel: proficiencyLevel, filteredKnowledge: filteredKnowledge}) AS proficiencyLevels
-
-MATCH (t:TSCKey) WHERE t.\`TSC Key ID\` = tscKeyID
-RETURN t, tscKeyID, proficiencyLevels;
-`;
+  MATCH (j:Job)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
+  WHERE j.jobid IN [1375, 1038, 757]
+  WITH jt.\`TSC Key ID\` AS TSCKeyID, k.\`TSC Title\` AS title
+  ORDER BY TSCKeyID DESC
+  
+  WITH COLLECT(DISTINCT TSCKeyID) AS requiredProficiency // Doesn't care what level of prof first
+  MATCH (n:TSCProficiency)
+  WHERE n.\`TSC Key ID\` IN requiredProficiency
+  
+  WITH n.\`TSC Key ID\` AS tscKeyID, n.\`Proficiency Level\` AS proficiencyLevel, COLLECT(DISTINCT n.\`Ability\`) AS distinctAbility
+  
+  // Group by TSC Key ID and Proficiency Level
+  WITH tscKeyID, proficiencyLevel, [x IN distinctAbility WHERE x IS NOT NULL | x] AS filteredAbility
+  
+  // Collect results by TSC Key ID
+  WITH tscKeyID, COLLECT({proficiencyLevel: proficiencyLevel, filteredAbility: filteredAbility}) AS proficiencyLevels
+  
+  MATCH (t:TSCKey) WHERE t.\`TSC Key ID\` = tscKeyID
+  RETURN t, tscKeyID, proficiencyLevels;
+  `;  
 read(query)
--- FYP Website/code/src/pages/api/technicalskills.js
+++ FYP Website/code/src/pages/api/technicalskills.js
@@ -3,35 +3,35 @@ import { read } from '../../lib/neo4j'
 // TODO:takes in all prevJob and curJob of user
 export default async function handler(req, res) {
   const query = `
-    MATCH (j:Job)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
-    WHERE j.jobid IN [1375, 1038, 757]
-    WITH jt.\`TSC Key ID\` AS TSCKeyID, k.\`TSC Title\` AS title
-    ORDER BY TSCKeyID DESC
-
-    WITH COLLECT(DISTINCT TSCKeyID) AS requiredProficiency // Doesn't care what level of prof first
-    MATCH (n:TSCProficiency)
-    WHERE n.\`TSC Key ID\` IN requiredProficiency
-
-    WITH n.\`TSC Key ID\` AS tscKeyID, n.\`Proficiency Level\` AS proficiencyLevel, COLLECT(DISTINCT n.\`Knowledge\`) AS distinctKnowledge
-
-    // Group by TSC Key ID and Proficiency Level
-    WITH tscKeyID, proficiencyLevel, [x IN distinctKnowledge WHERE x IS NOT NULL | x] AS filteredKnowledge
-
-    // Collect results by TSC Key ID
-    WITH tscKeyID, COLLECT({proficiencyLevel: proficiencyLevel, filteredKnowledge: filteredKnowledge}) AS proficiencyLevels
-
-    MATCH (t:TSCKey) WHERE t.\`TSC Key ID\` = tscKeyID
-    RETURN t, tscKeyID, proficiencyLevels;
-    `
+  MATCH (j:Job)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
+  WHERE j.jobid IN [1375, 1038, 757]
+  WITH jt.\`TSC Key ID\` AS TSCKeyID, k.\`TSC Title\` AS title
+  ORDER BY TSCKeyID DESC
+  
+  WITH COLLECT(DISTINCT TSCKeyID) AS requiredProficiency // Doesn't care what level of prof first
+  MATCH (n:TSCProficiency)
+  WHERE n.\`TSC Key ID\` IN requiredProficiency
+  
+  WITH n.\`TSC Key ID\` AS tscKeyID, n.\`Proficiency Level\` AS proficiencyLevel, COLLECT(DISTINCT n.\`Ability\`) AS distinctAbility
+  
+  // Group by TSC Key ID and Proficiency Level
+  WITH tscKeyID, proficiencyLevel, [x IN distinctAbility WHERE x IS NOT NULL | x] AS filteredAbility
+  
+  // Collect results by TSC Key ID
+  WITH tscKeyID, COLLECT({proficiencyLevel: proficiencyLevel, filteredAbility: filteredAbility}) AS proficiencyLevels
+  
+  MATCH (t:TSCKey) WHERE t.\`TSC Key ID\` = tscKeyID
+  RETURN t, tscKeyID, proficiencyLevels;
+  `;  
   const result = await read(query)
   const finalRes = result.records
     .map(record => record.toObject())
     .map(item => ({
       tscKeyID: item?.tscKeyID?.low,
-      proficiencyLevel: item?.proficiencyLevels.map(knowledge => {
+      proficiencyLevel: item?.proficiencyLevels.map(ability => {
         return {
-          proficiencyLevel: knowledge.proficiencyLevel.low,
-          filteredKnowledge: knowledge.filteredKnowledge.filter(knowledge => !!knowledge)
+          proficiencyLevel: ability.proficiencyLevel.low,
+          filteredAbility: ability.filteredAbility.filter(ability => !!ability)
         }
       }),
       tscDescrip: item.t.properties['TSC Description'],
