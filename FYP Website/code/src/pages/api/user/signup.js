f9cf3d5508ec09ab03631d1ade2218dad68ce8da
Chi Hui <chihui8199@gmail.com>
2023-09-08 16:50:13 +0800

    added endpoints for login

---
+++ FYP Website/code/src/pages/api/user/signup.js
@@ -0,0 +1,64 @@
+import { write, read } from '../../../lib/neo4j' // Import necessary functions for read and write operations
+import bcrypt from 'bcrypt'
+
+export default async function register(req, res) {
+  try {
+    const { email, password } = req.body
+    console.log('EMAIL', email, 'PASSWORD', password)
+
+    // Check if the user with the same email already exists
+    const existingUser = await getUserByEmail(email)
+    if (existingUser) {
+      return res.status(400).json({ message: 'User with this email already exists' })
+    }
+
+    // If the user doesn't exist, proceed to register
+    const hashedPassword = await bcrypt.hash(password, 10) // Hash the password
+
+    // Create the user in your database
+    const user = await createUser(email, hashedPassword)
+
+    return res.status(201).json({ message: 'User registered successfully', data: user })
+  } catch (e) {
+    console.error(e)
+    return res.status(500).json({ message: 'We are experiencing a server error, please try again later' })
+  }
+}
+const getUserByEmail = async email => {
+  const query = `
+      MATCH (user:User { email: $email })
+      RETURN user
+    `
+
+  const parameters = {
+    email: email
+  }
+
+  const result = await read(query, parameters) // Use your read function for querying
+
+  if (result && result.records.length > 0) {
+    const userRecord = result.records[0].get('user')
+    const userData = userRecord.properties // Extract user properties from the record
+    return userData
+  } else {
+    return null
+  }
+}
+
+const createUser = async (email, hashedPassword) => {
+  const query = `
+    CREATE (user:User {
+        email: $email,
+        password: $hashedPassword
+    })
+    RETURN user;
+  `
+
+  const parameters = {
+    email: email,
+    hashedPassword: hashedPassword
+  }
+
+  const user = await write(query, parameters) // Pass the parameters to your write function
+  return user
+}
