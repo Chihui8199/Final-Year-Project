f9014155d79a676605a801af8295eddd69952369
Chi Hui <chihui8199@gmail.com>
2023-09-12 18:47:15 +0800

    clean up theme with no errors

--- FYP Website/code/src/pages/_app.js
+++ FYP Website/code/src/pages/_app.js
@@ -56,12 +56,12 @@ const App = props => {
     <CacheProvider value={emotionCache}>
       <UserProvider>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+        <title>{`${themeConfig.templateName} - Skills Passport`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName} – Skills Passport`}
         />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <meta name='keywords' content='Jobs, Skills, Job Recommendations, Singapore Jobs' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
