845776dddac1fc058b6403be33618136f8f245d2
Chi Hui <chihui8199@gmail.com>
2023-09-13 23:18:31 +0800

    fix base line logic

--- FYP Website/code/src/pages/api/getAcquiredProficiency.js
+++ FYP Website/code/src/pages/api/getAcquiredProficiency.js
@@ -3,82 +3,92 @@ import { read } from '../../db/neo4j'
 export default async function handler(req, res) {
   const { email } = req.query
   const query = `
-  MATCH (u:User {email: "${email}"})
-  WITH u.state AS learnerState
-  
-  // Fetch the LearnerProfile based on the state property of the User
-  MATCH (lp:LearnerProfile) WHERE lp.uuid = learnerState
-  
-  // Fetch the desired jobID for the LearnerProfile
-  MATCH (lp)-[:HAS_TARGET_JOB_ROLE]->(j:Job)
-  WITH j, j.jobid AS desiredJobID, j.\`Job Role\` AS jobRole
-  
-  // Fetch required technical skills for the desired job
-  MATCH (j)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
-  WHERE j.jobid = desiredJobID
-  WITH jobRole, 
-      jt, k,
-      jt.\`TSC Key ID\` AS TSCKeyID,
-      k.\`TSC Title\` AS title,
-      jt.\`Proficiency Level\` AS jobProficiencyLevel
-  ORDER BY TSCKeyID DESC
-  
-  // Collect distinct TSC Key IDs and their proficiency levels
-  WITH jobRole, COLLECT({TSCKeyID: TSCKeyID, proficiency: jobProficiencyLevel}) AS requiredProficiencies
-  UNWIND requiredProficiencies AS proficiencyData
-  
-  // Fetch proficiency details for the TSC Key IDs
-  MATCH (n:TSCProficiency)
-  WHERE n.\`TSC Key ID\` = proficiencyData.TSCKeyID
-  WITH jobRole, 
-      n.\`TSC Key ID\` AS tscKeyID,
-      n.\`Proficiency Level\` AS proficiencyLevel,
-      n.\`Knowledge\` AS knowledge,
-      proficiencyData.proficiency AS jobProficiency
-  
-  // Group by keys and collect distinct knowledge
-  WITH jobRole, 
-      tscKeyID, 
-      proficiencyLevel, 
-      jobProficiency, 
-      COLLECT(DISTINCT knowledge) AS distinctAbility
-  
-  // Filter out null values from knowledge
-  WITH jobRole, 
-      tscKeyID, 
-      proficiencyLevel, 
-      jobProficiency, 
-      [x IN distinctAbility WHERE x IS NOT NULL | x] AS filteredKnowledge
-  
-  // Check for learner's proficiency level in the skill
-  OPTIONAL MATCH (lp:LearnerProfile)-[r:HAS_TECHNICAL_SKILLS]->(n)
-  WHERE r.\`TSC Key ID\` = tscKeyID
-  WITH jobRole, 
-      tscKeyID,
-      jobProficiency,
-      CASE WHEN lp IS NOT NULL THEN r.\`Proficiency Level\` ELSE -1 END AS userProficiency,
-      proficiencyLevel,
-      filteredKnowledge
-  
-  // Collect proficiency details
-  WITH jobRole,
-      tscKeyID, 
-      jobProficiency, 
-      userProficiency,
-      COLLECT(DISTINCT {proficiencyLevel: proficiencyLevel, filteredKnowledge: filteredKnowledge}) AS proficiencyLevels
-  
-  // Fetch the corresponding TSC Key details
-  MATCH (t:TSCKey) 
-  WHERE t.\`TSC Key ID\` = tscKeyID
-  
-  RETURN 
-      t, 
-      tscKeyID, 
-      jobProficiency, 
-      userProficiency, 
-      proficiencyLevels,
-      jobRole;
-  `
+MATCH (u:User {email: "${email}"})
+WITH u.state AS learnerState
+
+// Fetch the LearnerProfile based on the state property of the User
+MATCH (lp:LearnerProfile) WHERE lp.uuid = learnerState
+
+// Fetch the desired jobID for the LearnerProfile
+MATCH (lp)-[:HAS_TARGET_JOB_ROLE]->(j:Job)
+WITH j, j.jobid AS desiredJobID, j.\`Job Role\` AS jobRole
+
+// Fetch required technical skills for the desired job
+MATCH (j)-[:REQUIRE_TECHNICAL_SKILL]->(jt:JobTSC)-[:HAS_TSC_KEY]->(k:TSCKey)
+WHERE j.jobid = desiredJobID
+WITH jobRole, 
+    jt, k,
+    jt.\`TSC Key ID\` AS TSCKeyID,
+    k.\`TSC Title\` AS title,
+    jt.\`Proficiency Level\` AS jobProficiencyLevel
+ORDER BY TSCKeyID DESC
+
+// Collect distinct TSC Key IDs and their proficiency levels
+WITH jobRole, COLLECT({TSCKeyID: TSCKeyID, proficiency: jobProficiencyLevel}) AS requiredProficiencies
+UNWIND requiredProficiencies AS proficiencyData
+
+// Fetch proficiency details for the TSC Key IDs
+MATCH (n:TSCProficiency)
+WHERE n.\`TSC Key ID\` = proficiencyData.TSCKeyID
+WITH jobRole, 
+    n.\`TSC Key ID\` AS tscKeyID,
+    n.\`Proficiency Level\` AS proficiencyLevel,
+    n.\`Knowledge\` AS knowledgeDetails,
+    n.\`Ability\` AS abilityDetails,
+    n.\`Proficiency Description\` AS proficiencyDescription,
+    proficiencyData.proficiency AS jobProficiency
+
+// Group by keys and collect distinct knowledge
+WITH jobRole, 
+    tscKeyID, 
+    proficiencyLevel, 
+    jobProficiency, 
+    COLLECT(DISTINCT knowledgeDetails) AS distinctKnowledge,
+    COLLECT(DISTINCT abilityDetails) AS distinctAbility,
+    COLLECT(DISTINCT proficiencyDescription) AS distinctDescription
+
+// Filter out null values from knowledge
+WITH jobRole, 
+    tscKeyID, 
+    proficiencyLevel, 
+    jobProficiency, 
+    [x IN distinctKnowledge WHERE x IS NOT NULL | x] AS filteredKnowledge,
+    [x IN distinctAbility WHERE x IS NOT NULL | x] AS filteredAbility,
+    [x IN distinctDescription WHERE x IS NOT NULL | x] AS filteredDescription
+
+// Check for learner's proficiency level in the skill
+OPTIONAL MATCH (lp:LearnerProfile)-[r:HAS_TECHNICAL_SKILLS]->(n)
+WHERE r.\`TSC Key ID\` = tscKeyID
+WITH jobRole, 
+    tscKeyID,
+    jobProficiency,
+    CASE WHEN lp IS NOT NULL THEN r.\`Proficiency Level\` ELSE -1 END AS userProficiency,
+    proficiencyLevel,
+    filteredKnowledge,
+    filteredAbility,
+    filteredDescription
+
+// Collect proficiency details
+WITH jobRole,
+    tscKeyID, 
+    jobProficiency, 
+    userProficiency,
+    COLLECT(DISTINCT {proficiencyLevel: proficiencyLevel, filteredKnowledge: filteredKnowledge, filteredAbility: filteredAbility, filteredDescription: filteredDescription}) AS proficiencyDetails
+
+// Fetch the corresponding TSC Key details
+MATCH (t:TSCKey) 
+WHERE t.\`TSC Key ID\` = tscKeyID
+
+RETURN 
+    t, 
+    tscKeyID, 
+    jobProficiency, 
+    userProficiency, 
+    proficiencyDetails,
+    jobRole;
+`;
+
+
 
   // Now you can use the 'query' constant in your JavaScript program
 
@@ -89,17 +99,41 @@ export default async function handler(req, res) {
 }
 
 // TODO: rename filteredKnowledge to fitleredAbility
+// const formatData = result => {
+//   const finalRes = result.records
+//     .map(record => record.toObject())
+//     // .map(item => ({
+//     //   jobTitle: item.jobRole,
+//     //   tscKeyID: item?.tscKeyID?.low,
+//     //   proficiencyLevel: item?.proficiencyLevels
+//     //     .map(ability => {
+//     //       return {
+//     //         proficiencyLevel: ability.proficiencyLevel.low,
+//     //         filteredAbility: ability?.filteredAbility.filter(ability => !!ability)
+//     //       }
+//     //     })
+//     //     .sort((a, b) => a.proficiencyLevel - b.proficiencyLevel),
+//     //   tscDescrip: item.t.properties['TSC Description'],
+//     //   tscCategory: item.t.properties['TSC Category'],
+//     //   tscTitle: item.t.properties['TSC Title'],
+//     //   jobRequiredProficiency: item.jobProficiency.low,
+//     //   userAcquiredProficiency: item.userProficiency.low
+//     // }))
+//   return finalRes
+// }
 const formatData = result => {
   const finalRes = result.records
     .map(record => record.toObject())
     .map(item => ({
       jobTitle: item.jobRole,
       tscKeyID: item?.tscKeyID?.low,
-      proficiencyLevel: item?.proficiencyLevels
-        .map(ability => {
+      proficiencyDetails: item?.proficiencyDetails
+        .map(detail => {
           return {
-            proficiencyLevel: ability.proficiencyLevel.low,
-            filteredAbility: ability?.filteredKnowledge.filter(ability => !!ability)
+            proficiencyLevel: detail.proficiencyLevel.low,
+            filteredKnowledge: detail?.filteredKnowledge.filter(knowledge => !!knowledge),
+            filteredAbility: detail?.filteredAbility.filter(ability => !!ability),
+            filteredDescription: detail?.filteredDescription.filter(description => !!description)
           }
         })
         .sort((a, b) => a.proficiencyLevel - b.proficiencyLevel),
@@ -109,5 +143,6 @@ const formatData = result => {
       jobRequiredProficiency: item.jobProficiency.low,
       userAcquiredProficiency: item.userProficiency.low
     }))
-  return finalRes
+  return finalRes;
 }
+
--- FYP Website/code/src/views/timelineaccordion/index.js
+++ FYP Website/code/src/views/timelineaccordion/index.js
@@ -25,27 +25,36 @@ const buttonBaseStyles = {
   cursor: 'pointer'
 }
 
-
-
 export default function TimelineAccordion(props) {
   const item = props.data
-
-    // Find proficiency level to set intial card conten
-  const getInitialCardContent = () => {
-      // if userProficiency is -1 then set to the lowest proficiency 
-      // else set to the lowest proficiency level in the list
-      const userProficiency = item.userAcquiredProficiency
-      if (userProficiency > 0) {
-      const abilityDesc = item['proficiencyLevel'].find(item => item['proficiencyLevel'] === userProficiency)
+  // Find proficiency level to set intial card conten
+  const getInitialAbilityCardContent = () => {
+    // if userProficiency is -1 then set to the lowest proficiency
+    // else set to the lowest proficiency level in the list
+    const userProficiency = item.userAcquiredProficiency
+    if (userProficiency > 0) {
+      const abilityDesc = item['proficiencyDetails'].find(item => item['proficiencyLevel'] === userProficiency)
         ?.filteredAbility || ['Description not available']
-        return abilityDesc
-      } else {
-        return item.proficiencyLevel[0].filteredAbility
-      }
+      return abilityDesc
+    } else {
+      return item.proficiencyDetails[0].filteredAbility
     }
+  }
+  const getInitialKnowCardContent = () => {
+    // if userProficiency is -1 then set to the lowest proficiency
+    // else set to the lowest proficiency level in the list
+    const userProficiency = item.userAcquiredProficiency
+    if (userProficiency > 0) {
+      const knowDesc = item['proficiencyDetails'].find(item => item['proficiencyLevel'] === userProficiency)
+      ?.filteredKnowledge || ['Description not available']
+      return knowDesc
+    } else {
+      return item.proficiencyDetails[0].filteredKnowledge
+    }
+  }
   // State to track which button is clicked and its associated content
-  const [activeButtonIndex, setActiveButtonIndex] = React.useState(null)
-  const [activeCardContent, setActiveCardContent] = React.useState(getInitialCardContent())
+  const [abilityCardContent, setAbilityCardContent] = React.useState(getInitialAbilityCardContent())
+  const [knowCardContent, setKnowCardContent] = React.useState(getInitialKnowCardContent())
   const [expanded, setExpanded] = React.useState(false)
 
   const handleAccordionToggle = () => {
@@ -53,29 +62,31 @@ export default function TimelineAccordion(props) {
   }
   // Find index for that proficiency level
   const findProficiencyLevel = proficiencyLevel => {
-    const levels = item.proficiencyLevel.map(item => item.proficiencyLevel)
+    const levels = item.proficiencyDetails.map(item => item.proficiencyLevel)
     const index = levels.findIndex(item => item === proficiencyLevel)
     return index // TODO: adds error handing: return -1 if prof level not inside
   }
 
   // Event handler for the TimelineDot click event
   const handleDotClick = (index, proficiencyLevel) => {
-    setActiveButtonIndex(index)
     // TODO: maybe you want to set the proficiency level here
-    const abilityDesc = item['proficiencyLevel'].find(item => item['proficiencyLevel'] === proficiencyLevel)
+    const abilityDesc = item['proficiencyDetails'].find(item => item['proficiencyLevel'] === proficiencyLevel)
       ?.filteredAbility || ['Description not available']
-    setActiveCardContent(abilityDesc)
+      const knowDesc = item['proficiencyDetails'].find(item => item['proficiencyLevel'] === proficiencyLevel)
+      ?.filteredKnowledge || ['Description not available']
+    setKnowCardContent(knowDesc)
+    setAbilityCardContent(abilityDesc)
   }
 
   // TODO: make further improvements to the colour of the dots
   // Base on certain conditions return the dot colour
   const getDotColour = index => {
-    console.log("item", item)
+    console.log('item', item)
     const userProficiency = item.userAcquiredProficiency
     const userProficiencyIndex = findProficiencyLevel(userProficiency)
     const jobProficiency = item.jobRequiredProficiency
     const jobRequiredProficiencyIndex = findProficiencyLevel(jobProficiency)
-    if (userProficiency === -1){
+    if (userProficiency === -1) {
       // Make all dots till job required proficiency level red
       if (index <= jobRequiredProficiencyIndex) {
         return 'error'
@@ -85,7 +96,7 @@ export default function TimelineAccordion(props) {
       if (index <= userProficiencyIndex) {
         return 'success'
       }
-      if (userProficiencyIndex < index && index <= jobRequiredProficiencyIndex){
+      if (userProficiencyIndex < index && index <= jobRequiredProficiencyIndex) {
         return 'error'
       }
     }
@@ -96,13 +107,12 @@ export default function TimelineAccordion(props) {
     }
     if (userProficiency == jobProficiency) {
       if (index <= jobRequiredProficiencyIndex) {
-      return 'success'
+        return 'success'
       }
     }
-    
+
     return 'secondary'
   }
-
 
   return (
     <Accordion expanded={expanded} onChange={handleAccordionToggle}>
@@ -119,7 +129,7 @@ export default function TimelineAccordion(props) {
                 }
               }}
             >
-              {item.proficiencyLevel.map((event, index) => (
+              {item.proficiencyDetails.map((event, index) => (
                 <TimelineItem key={index}>
                   <TimelineOppositeContent color='textSecondary'>
                     Level {event.proficiencyLevel}
@@ -133,19 +143,30 @@ export default function TimelineAccordion(props) {
                     </ButtonBase>
                     <TimelineConnector />
                   </TimelineSeparator>
-                  <TimelineContent>Change to Prof Decript</TimelineContent>
+                  <TimelineContent>
+                    <Typography variant='body2'>{event.filteredDescription}</Typography>
+                  </TimelineContent>
                 </TimelineItem>
               ))}
             </Timeline>
           </CardContent>
           <CardContent style={{ flex: 0.8, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <ul>
+               <Typography variant='h6'>Ability Required</Typography>
               <Typography variant='body2'>
-                {activeCardContent.map((item, index) => (
+                {abilityCardContent.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+        
+              </Typography>
+              <Typography variant='h6' style={{ marginTop: '20px' }}>Knowledge Required</Typography>
+              <Typography variant='body2'>
+                {knowCardContent.map((item, index) => (
                   <li key={index}>{item}</li>
                 ))}
               </Typography>
             </ul>
+            
           </CardContent>
         </Card>
       </AccordionDetails>
