abb084f4e11cf47672d8a6005cc68ca975926d6c
Chi Hui <chihui8199@gmail.com>
2023-09-27 16:16:12 +0800

    final recc page

--- FYP Website/code/src/pages/pages/recommendations/index.js
+++ FYP Website/code/src/pages/pages/recommendations/index.js
@@ -14,6 +14,11 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList } from 'react-window';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function TextMobileStepper() {
   const theme = useTheme();
@@ -21,8 +26,27 @@ export default function TextMobileStepper() {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const maxSteps = jobs.length;
+  const [recc, setAlgo] = React.useState('content');
+
 
   //TODO: uncomment this later
+
+
+  const { user } = useUserContext();
+  //const user = { email: 'isabelle@vincere.health' };
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+
+  const handleChange = (event) => {
+    setAlgo(event.target.value);
+  };
 
   function renderRow(props) {
     const { index, style } = props;
@@ -43,28 +67,17 @@ export default function TextMobileStepper() {
     );
   }
 
-  //const { user } = useUserContext();
-  const user = { email: 'isabelle@vincere.health' };
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
   useEffect(() => {
     const fetchJobs = async () => {
       if (!user) return;
       try {
-        // TODO: all for dropdown to choose strategy type
         const response = await fetch(
-          `/api/recommendation/getJobsRecc?user_id=${user.email}&strategy_type=embedding`,
+          `/api/recommendation/getJobsRecc?user_id=${user.email}&strategy_type=${recc}`,
           {
             method: 'GET',
           },
         );
+       
         const data = await response.json();
         setJobs(data);
       } catch (error) {
@@ -76,98 +89,123 @@ export default function TextMobileStepper() {
     };
 
     fetchJobs();
-  }, [user]);
+  }, [user, recc]);
 
   return (
+    
     <div>
       {loading ? (
         <LoadingCard />
       ) : (
-        <Paper sx={{ elevation: 3 }}>
-          <Box sx={{ p: 2, mx: 'auto' }}>
-            <Paper
-              square
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                height: 50,
-                pl: 2,
-                bgcolor: 'background.default',
-              }}
-            >
-              <Typography variant="h5">{jobs[activeStep].jobRole}</Typography>
-            </Paper>
-            <Box sx={{ p: 2 }}>
-              <Typography variant="subtitle1" color="primary">
-                Job Description
-              </Typography>
-              <Typography
-                variant="caption"
-                sx={{ marginTop: 3, textAlign: 'justify' }}
+        <div>
+          <div key={jobs.length}></div>
+          <div>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-simple-select-helper-label">
+                Algorithm
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={recc}
+                label="Algorithm"
+                onChange={handleChange}
               >
-                {jobs[activeStep].jobRoleDescription}
-              </Typography>
-            </Box>
-            <Box sx={{ p: 2 }}>
-              <Typography variant="subtitle1" color="primary" sx={{ px: 2 }}>
-                Job Tasks
-              </Typography>
-              <Box
+                <MenuItem value={'content'}>Content</MenuItem>
+                <MenuItem value={'embedding'}>Embedding</MenuItem>
+                <MenuItem value={'collaborative'}>Collaborative</MenuItem>
+              </Select>
+              <FormHelperText>
+                Choose the recommendation Algorithm
+              </FormHelperText>
+            </FormControl>
+          </div>
+          <Paper sx={{ elevation: 3 }}>
+            <Box sx={{ p: 2, mx: 'auto' }}>
+              <Paper
+                square
                 sx={{
-                  width: '100%',
-                  maxWidth: '100%',
-                  bgcolor: 'background.paper',
-                  mb: 10,
+                  display: 'flex',
+                  alignItems: 'center',
+                  height: 50,
+                  pl: 2,
+                  bgcolor: 'background.default',
                 }}
               >
-                <FixedSizeList
-                  height={250}
-                  width={'100%'}
-                  itemSize={46}
-                  itemCount={jobs[activeStep].keyTasksList.length}
-                  overscanCount={5}
+                <Typography variant="h5">{jobs[activeStep].jobRole}</Typography>
+              </Paper>
+              <Box sx={{ p: 2 }}>
+                <Typography variant="subtitle1" color="primary">
+                  Job Description
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{ marginTop: 3, textAlign: 'justify' }}
                 >
-                  {renderRow}
-                </FixedSizeList>
+                  {jobs[activeStep].jobRoleDescription}
+                </Typography>
               </Box>
-            </Box>
+              <Box sx={{ p: 2 }}>
+                <Typography variant="subtitle1" color="primary" sx={{ px: 2 }}>
+                  Job Tasks
+                </Typography>
+                <Box
+                  sx={{
+                    width: '100%',
+                    maxWidth: '100%',
+                    bgcolor: 'background.paper',
+                    mb: 10,
+                  }}
+                >
+                  <FixedSizeList
+                    height={250}
+                    width={'100%'}
+                    itemSize={46}
+                    itemCount={jobs[activeStep].keyTasksList.length}
+                    overscanCount={5}
+                  >
+                    {renderRow}
+                  </FixedSizeList>
+                </Box>
+              </Box>
 
-            <MobileStepper
-              variant="text"
-              steps={maxSteps}
-              position="static"
-              activeStep={activeStep}
-              nextButton={
-                <Button
-                  size="small"
-                  onClick={handleNext}
-                  disabled={activeStep === maxSteps - 1}
-                >
-                  Next
-                  {theme.direction === 'rtl' ? (
-                    <KeyboardArrowLeft />
-                  ) : (
-                    <KeyboardArrowRight />
-                  )}
-                </Button>
-              }
-              backButton={
-                <Button
-                  size="small"
-                  onClick={handleBack}
-                  disabled={activeStep === 0}
-                >
-                  {theme.direction === 'rtl' ? (
-                    <KeyboardArrowRight />
-                  ) : (
-                    <KeyboardArrowLeft />
-                  )}
-                  Back
-                </Button>
-              }
-            />
-          </Box>
-        </Paper>
+              <MobileStepper
+                variant="text"
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                  <Button
+                    size="small"
+                    onClick={handleNext}
+                    disabled={activeStep === maxSteps - 1}
+                  >
+                    Next
+                    {theme.direction === 'rtl' ? (
+                      <KeyboardArrowLeft />
+                    ) : (
+                      <KeyboardArrowRight />
+                    )}
+                  </Button>
+                }
+                backButton={
+                  <Button
+                    size="small"
+                    onClick={handleBack}
+                    disabled={activeStep === 0}
+                  >
+                    {theme.direction === 'rtl' ? (
+                      <KeyboardArrowRight />
+                    ) : (
+                      <KeyboardArrowLeft />
+                    )}
+                    Back
+                  </Button>
+                }
+              />
+            </Box>
+          </Paper>
+        </div>
       )}
     </div>
   );
