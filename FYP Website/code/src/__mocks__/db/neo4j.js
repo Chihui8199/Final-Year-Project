65c5502bb4ae078427eff2180e61cdd0b5ef5ba0
Chi Hui <chihui8199@gmail.com>
2023-10-17 00:46:56 +0800

    add one unit test for demo LOL

---
+++ FYP Website/code/src/__mocks__/db/neo4j.js
@@ -0,0 +1,4 @@
+
+export const read = jest.fn();
+
+export const write = jest.fn();
---
+++ FYP Website/code/src/__tests__/getAllJobs.test.js
@@ -0,0 +1,80 @@
+jest.mock('../db/neo4j', () => ({
+    read: jest.fn(),
+}));
+
+// Mocking middleware to skip JWT verification for the tests.
+jest.mock('../middlewares/verifyJWT', () => (req, res, next) => next());
+
+import { read } from '../db/neo4j';
+import handler from '../pages/api/jobs/getAllJobs';
+
+describe('Job API Handler', () => {
+    // Clean up and reset mocks after each test to ensure no test side effects.
+    beforeEach(() => {
+        jest.clearAllMocks();  
+    });
+
+    it('returns reformatted data on success', async () => {
+        // Arrange
+        const mockRecords = [
+            {
+                toObject: () => ({
+                    n: {
+                        properties: {
+                            jobid: { low: 1 },
+                            'Job Role': 'Developer',
+                            Sector: 'IT',
+                            Track: 'Backend',
+                        },
+                        elementId: 'some-id'
+                    }
+                })
+            }
+        ];
+        
+        // Mock the `read` function to return our mock records.
+        read.mockResolvedValueOnce({ records: mockRecords });
+
+        const mockReq = {};
+
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        // Act
+        await handler(mockReq, mockRes);
+
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith({
+            data: [{
+                JobId: 1,
+                'Job Role': 'Developer',
+                Sector: 'IT',
+                Track: 'Backend',
+                ElementID: 'some-id',
+                DisplayName: 'Developer - IT - Backend'
+            }]
+        });
+    });
+
+    it('returns 500 on error', async () => {
+        // Arrange
+        read.mockRejectedValueOnce(new Error('Database error'));
+
+        const mockReq = {};
+
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            send: jest.fn()
+        };
+
+        // Act
+        await handler(mockReq, mockRes);
+
+        // Assert
+        expect(mockRes.status).toHaveBeenCalledWith(500);
+        expect(mockRes.send).toHaveBeenCalledWith('Internal server error');
+    });
+});
